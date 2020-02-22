9c0d3423551a652354073f5f87df6a8c64f15418
Samantha Hu <machine411r@gmail.com>
2020-02-22 15:31:10 +1030

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay!Printing.')
+print("I'd much rather you'not'.")
+print('i"said"do not touch this.')
+###study drills
+#awk 'NR==2' ex1.py|python3
+#sed -n '2 p' ex1.py |python3
+#awk 'NR>=2 && NR<=4' ex1.py | python3
+#=sed -n '2,5 p' ex1.py |python3
