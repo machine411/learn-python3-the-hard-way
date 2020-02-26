dfbb314f58a25c75e8de86ae568815644ac19fb5
Samantha Hu <machine411r@gmail.com>
2020-02-26 21:41:29 +1030

    commit4

---
+++ ex25_1.py
@@ -0,0 +1,13 @@
+import ex25
+sentence = "All good thins come to those who wait."
+words = ex25.break_words(sentence)
+
+sorted_words = ex25.sort_words(words)
+
+ex25.print_first_word(words)
+ex25.print_last_word(sorted_words)
+
+sorted_words = ex25.sort_sentence(sentence)
+
+ex25.print_first_and_last(sentence)
+ex25.print_first_and_last_sorted(sentence)
