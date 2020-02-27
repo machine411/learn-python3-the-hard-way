7c39f1aca94b2a32a83ee2a292d3a6780c685711
Samantha Hu <machine411r@gmail.com>
2020-02-27 23:19:21 +1030

    commit_27/02

--- ex29.py
+++ ex29.py
@@ -6,4 +6,21 @@ if people < cats:
     print("Too many cats! The world is doomed!")
 
 if people > cats:
-    print("Not many cats! The world is saved!")
+    print("Not many cats! The world is saved to ")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs +=5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
