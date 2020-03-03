ae7472d689af14f35bd8f7cd6742d5fd53368349
Samantha Hu <machine411r@gmail.com>
2020-03-03 19:43:51 +1030

    commit 5

--- ex43.py
+++ ex43.py
@@ -1,4 +1,4 @@
-from sys import exist
+from sys import exit
 from random import randint
 from textwrap import dedent
 
@@ -12,77 +12,83 @@ class Engine(object):
 
     def __init__(self,scene_map):
         self.scene_map = scene_map
+
     def play(self):
         current_scene = self.scene_map.opening_scene()
         last_scene = self.scene_map.next_scene('finished')
 
         while current_scene != last_scene:
-            next_scene_name = current_scene.enter()
-            current_scene = self.scene_map.next_scene(next_scene_names)
-
-        current_scene.ener()
+            if current_scene != None:
+                next_scene_name = current_scene.enter()
+                current_scene = self.scene_map.next_scene(next_scene_name)
+            else:
+                break
+        #current_scene.enter()
 
 class Death(Scene):
     quips = [
-     "You died. You kinda suck at this.",
-     "Your Mom would be proud...if she were smarter.", "Such a luser.",
-     "I have a small puppy that's better at this.", "You're worse than your Dad's jokes."
-      ]
+        "You died. You kinda suck at this.",
+        "Your Mom would be proud...if she were smarter.", "Such a luser.",
+        "I have a small puppy that's better at this.", "You're worse than your Dad's jokes."
+    ]
+
     def enter(self):
         print(Death.quips[randint(0, len(self.quips)-1)])
         exit(1)
+
 class CentralCorridor(Scene):
+
     def enter(self):
         print(dedent("""
-               The Gothons of Planet Percal #25 have invaded your ship and
-               destroyed your entire crew. You are the last surviving
-                member and your last mission is to get the neutron destruct
-               bomb from the Weapons Armory, put it in the bridge, and
-               blow the ship up after getting into an escape pod.
+              The Gothons of Planet Percal #25 have invaded your ship and
+              destroyed your entire crew. You are the last surviving
+              member and your last mission is to get the neutron destruct
+              bomb from the Weapons Armory, put it in the bridge, and
+              blow the ship up after getting into an escape pod.
 
-                 You're running down the central corridor to the Weapons
-                 Armory when a Gothon jumps out, red scaly skin, dark grimy
-                teeth, and evil clown costume flowing around his hate
-                filled body. He's blocking the door to the Armory and
-                about to pull a weapon to blast you.
-                """))
-            action = input("> ")
+              You're running down the central corridor to the Weapons
+              Armory when a Gothon jumps out, red scaly skin, dark grimy
+             teeth, and evil clown costume flowing around his hate
+             filled body. He's blocking the door to the Armory and
+             about to pull a weapon to blast you.
+              """))
+        action = input("> ")
+        if action == "shoot!":
+            print(dedent("""
+                  Quick on the draw you yank out your blaster and fire
+                  it at the Gothon. His clown costume is flowing and
+                  moving around his body, which throws off your aim.
+                  Your laser hits his costume but misses him entirely.
+                  This completely ruins his brand new costume his mother
+                  balabala
+                  """))
+            return 'death'
 
-            if action == "shoot!":
-                print(dedent("""
-                Quick on the draw you yank out your blaster and fire
-                it at the Gothon. His clown costume is flowing and
-                moving around his body, which throws off your aim.
-                Your laser hits his costume but misses him entirely.
-                This completely ruins his brand new costume his mother
-                balabala
-                """))
-                return 'death'
-            elif action == "dodge!":
-                print(dedent("""
-                     Like a world class boxer you dodge, weave, slip and
-                     slide right as the Gothon's blaster cranks a laser
-                     past your head. In the middle of your artful dodge
-                     your foot slips and you bang your head on the metal
-                     wall and pass out. You wake up shortly after only to
-                     die as the Gothon stomps on your head and eats you
-                     """))
-                    return 'death'
-            elif action == "tell a joke":
-                print(dedent("""
-                Lucky for you they made you learn Gothon insults in
-                the academy. You tell the one Gothon joke you know:
-                Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
-                fur fvgf nebhaq gur ubhfr. The Gothon stops, tries
-                not to laugh, then busts out laughing and can't move.
-                While he's laughing you run up and shoot him square in                      the head putting him down, then jump through the
-                Weapon Armory door.
-                """))
-                return 'leaser_weapon_armory'
-            else:
-                print("DOES NOT COMPUTE!")
-                return 'central_corridor'
+        elif action == "dodge!":
+             print(dedent("""
+                   Like a world class boxer you dodge, weave, slip and
+                   slide right as the Gothon's blaster cranks a laser
+                   past your head. In the middle of your artful dodge
+                   your foot slips and you bang your head on the metal
+                   wall and pass out. You wake up shortly after only to
+                   die as the Gothon stomps on your head and eats you
+                   """))
+             return 'death'
+        elif action == "tell a joke":
+             print(dedent("""
+                 Lucky for you they made you learn Gothon insults in
+                 the academy. You tell the one Gothon joke you know:
+                 Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr,
+                 fur fvgf nebhaq gur ubhfr. The Gothon stops, tries
+                 not to laugh, then busts out laughing and can't move.                 While he's laughing you run up and shoot him square in                      the head putting him down, then jump through the
+                 Weapon Armory door.
+                 """))
+             return 'leaser_weapon_armory'
+        else:
+            print("DOES NOT COMPUTE!")
+            return 'central_corridor'
 class LaserWeappnArmory(Scene):
+
     def enter(self):
         print(dedent("""
         You do a dive roll into the Weapon Armory, crouch and scan
@@ -113,13 +119,15 @@ class LaserWeappnArmory(Scene):
             return 'the_bridge'
         else:
             print(dedent("""
-            the lock buzzes one last
-            sickening
-            togther.
-            gothons blow up
-            """))
+                the lock buzzes one last
+                sickening
+                togther.
+                gothons blow up
+                """))
+            return 'death'
 
 class TheBridge(Scene):
+
     def enter(self):
         print(dedent("""
               You burst onto the bridge
@@ -159,17 +167,19 @@ class TheBridge(Scene):
             print("DOES NOT COMPUTE!")
             return "the_bridge"
 
-class EsacpePod(Scene):
+class EscapePod(Scene):
+
     def enter(self):
         print(dedent("""
-        You rush through the ship
-        the escape pod before the whole ship
-        like hardly any gothons are on the ship
-        clear of interference. you get to the
-        escape pods, and now need to pick one # TODO:
-        them could be damaged but you don't have
-        There's 5 pods,which one do you take?
-        """))
+            You rush through the ship
+            the escape pod before the whole ship
+            like hardly any gothons are on the ship
+            clear of interference. you get to the
+            escape pods, and now need to pick one # TODO:
+            them could be damaged but you don't have
+            There's 5 pods,which one do you take?
+            """))
+
         good_pod = randint(1,5)
         guess = input("[pod #]> ")
 
@@ -209,12 +219,15 @@ class Map(object):
         'death':Death(),
         'finished':Finished(),
     }
-    def __init__(self,start_scene):
+    def __init__(self, start_scene):
         self.start_scene = start_scene
-    def next_scene(self,scene_name):
-        pass
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
 
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
